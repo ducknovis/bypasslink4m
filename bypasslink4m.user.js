73014a1aab1e99f2d3e97477e6c76c97d35dc6e3
ducknovis <liminhomcc@gmail.com>
2025-03-12 21:44:10 +0700

    Add files via upload

---
+++ bypasslink4m.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Link4m Bypass by Ducknovis
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  sờ ki bi đi toi lét
+// @author       Ducknovis
+// @match        *://link4m.com/go/*
+// @grant        none
+// @icon         https://tr.rbxcdn.com/180DAY-433e973b5fbc1e8d5cdebd0c6be03472/420/420/Decal/Webp/noFilter
+// @require      https://github.com/ducknovis/bypasslink4m/raw/refs/heads/main/bypasslink4m.js
+// ==/UserScript==
